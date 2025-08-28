d031520c480a63cd19b3eb01bf7bfc693d347d15
Matheus <matheusandrade.ma2003@gmail.com>
2025-08-28 20:20:50 -0300

    feat: optimize

--- cap4-extra/exercicio2.py
+++ cap4-extra/exercicio2.py
@@ -6,10 +6,9 @@ dados = dataset[1:]
 
 regions = dados[:, 1]
 
-unique_regions, counts = np.unique(regions, return_counts=True)
+unique_regions = np.unique(regions)
 
 unique_regions_count = len(unique_regions)
 
 print("Quantidade de regiões diferentes: ", len(unique_regions))
-for c in range(unique_regions_count):
-    print("Região: ", unique_regions[c], ", quantidade que aparece: ", counts[c], " vezes")
+print(unique_regions)
